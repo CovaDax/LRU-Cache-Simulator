18df35b811cdf45742275f468b19ee3d95f01746
Andrew Gari <covadax@verizon.net>
2013-03-23 04:31:18 -0400

    Added input prompts for Cache, Block and Associativity. Needs to be reworked so it's a bit more elegant. Need to add Try-Catch so the user cannot input anything other than the given sizes.

--- Computer Architecture Project/src/Cache.java
+++ Computer Architecture Project/src/Cache.java
@@ -14,7 +14,7 @@ public class Cache {
     private int cacheSize;
     private int blockSize;
     private int numSets = 0;
-    private int setAssociativity;
+    private int Associativity;
     
     int[] setsInCache = new int[numSets];
     
@@ -32,15 +32,25 @@ public class Cache {
     
     public int getBlocksize() {
        
-        System.out.println("\tenter a block size: ");
+        System.out.println("Chooe Block Size: '8-Bytes', '16-Bytes' or '32-Bytes'");
                 
         Scanner in = new Scanner(System.in);
         blockSize = in.nextInt();
-        
-        
         return blockSize; 
-        
+    }
+    
+    public int getCachesize() {
+        System.out.println("Chooe Cache Size: '1024-Bytes', '2048-Bytes', '4096-Bytes' or '8192-Bytes'");
+        Scanner in =  new Scanner(System.in);
+        cacheSize = in.nextInt();
+        return cacheSize;
     }
      
+    public int getAssociativity() {
+        System.out.println("Please choose Associativity: 0 for 'Direct Mapped', 2 for 'TwoWay', 4 for 'FourWay' or 8 for 'EightWay'");
+        Scanner in = new Scanner(System.in);
+        Associativity = in.nextInt();
+        return Associativity;
+    }
     
 }
--- Computer Architecture Project/src/cacheSimulator.java
+++ Computer Architecture Project/src/cacheSimulator.java
@@ -5,7 +5,7 @@
 
 /**
  *
- * @author Andrew Meyers
+ * @author Andrew Meyers, Andrew Gari
  */
 
 import java.io.*;
@@ -14,36 +14,47 @@ import java.util.*;
 
 public class cacheSimulator {
     
+    /*Creates two variables to determine how many times the cache has been
+     * accessed so far, and one for how many hits you have so far.
+     */
     private int accessCount;
     private int hitCount;
-    
-    public cacheSimulator() {
-        
-        
-    }
     
     
     public static void main(String[] args) {
         
         cacheSimulator CS = new cacheSimulator();
-        Cache c = new Cache();
-        
-        System.out.println("Test");
+        Cache c = new Cache(); //create a class object
         
         try {
-        
         FileInputStream FS = new FileInputStream("trace");
         InputStreamReader IS = new InputStreamReader(FS);
-        
-        
         }
-        
+       
         catch(Exception e) {
-            
-            
+                    
         }
         
-        System.out.println("\t" + " " +  c.getBlocksize());
+        System.out.println("\t" + " " + c.getCachesize() + "-Bytes.");
+        System.out.println("\t" + " " +  c.getBlocksize() + "-Bytes.");
+        int Associativity = c.getAssociativity();
+        if (Associativity==0){
+            System.out.println("\t" + " Direct Mapped Cache");
+        }
+        else if (Associativity==2){
+            System.out.println("\t" + "Two-Way Associativity");
+        }
+        else if (Associativity==4){
+            System.out.println("\t" + "Four-Way Associativity");
+        }
+        else if (Associativity==8){
+            System.out.println("\t" + "Eight-Way Associativity");
+        }
+        else{
+            Associativity=c.getAssociativity();
+        }
+        
+            
   
         
         
