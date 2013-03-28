017efa3b5023409b9314606aa272dc9511870a30
Andrew Gari <covadax@verizon.net>
2013-03-27 22:07:42 -0400

    Completely Working Relative Path

--- Computer Architecture Project/src/readFile.java
+++ Computer Architecture Project/src/readFile.java
@@ -10,12 +10,11 @@
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Path;
+
 
 
 public class readFile {
@@ -23,20 +22,20 @@ public class readFile {
     }
     
     public String[] openFile() throws IOException{
-        String file = readFile.class.getClassLoader().getResource("trace").toString();
+        String file2="C:\\Users\\Gerald XXI\\workspace\\Java\\Computer_Architecture_Project\\Computer Architecture Project\\src\\trace";
+        String file="src\\trace";
+        //String file2 = readFile.class.getClassLoader().getResource("C:\\Users\\Gerald XXI\\workspace\\Java\\Computer_Architecture_Project\\Computer Architecture Project\\src\\trace").toString();
         String line = null;
         List<String> lines = new ArrayList<String>();
         
         try{
             FileReader fr = new FileReader(file);
-            BufferedReader br=new BufferedReader(fr);
-            
-            while((line=br.readLine()) != null){
-            lines.add(line);
-            System.out.println(line);
+            try (BufferedReader br = new BufferedReader(fr)) {
+                while((line=br.readLine()) != null){
+                lines.add(line);
+                System.out.println(line);
+                }
             }
-            
-            br.close();
         }
         
         catch(FileNotFoundException ex){
